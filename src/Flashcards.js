8c500f77c8e7b5c0958f55e9065b1b096a408ac7
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-09 22:12:45 +0200

    Added quiz mode

--- src/Flashcards.js
+++ src/Flashcards.js
@@ -4,6 +4,10 @@ import Flashcard from "./Flashcard";
 const Flashcards = ({ vocabulary }) => {
   const [learnedWords, setLearnedWords] = useState([]);
   const [shuffledVocabulary, setShuffledVocabulary] = useState([]);
+  const [quizMode, setQuizMode] = useState(false);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [choices, setChoices] = useState([]);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     const storedLearnedWords = localStorage.getItem("learnedWords");
@@ -16,14 +20,70 @@ const Flashcards = ({ vocabulary }) => {
     setShuffledVocabulary(shuffleArray(vocabulary));
   }, [vocabulary]);
 
+  useEffect(() => {
+    if (quizMode) {
+      const correctWord = shuffledVocabulary[currentWordIndex];
+      const incorrectWords = shuffleArray(
+        vocabulary.filter((item) => item.word !== correctWord.word)
+      ).slice(0, 3);
+      setChoices(shuffleArray([correctWord, ...incorrectWords]));
+    }
+  }, [quizMode, currentWordIndex]);
+
   const handleLearnWord = (word) => {
     const newLearnedWords = [...learnedWords, word];
     setLearnedWords(newLearnedWords);
     localStorage.setItem("learnedWords", JSON.stringify(newLearnedWords));
   };
 
+  const handleStartQuiz = () => {
+    setQuizMode(true);
+    setCurrentWordIndex(0);
+    setScore(0);
+  };
+
+  const handleAnswer = (word) => {
+    if (word === shuffledVocabulary[currentWordIndex].word) {
+      setScore(score + 1);
+    }
+    setCurrentWordIndex(currentWordIndex + 1);
+  };
+
+  if (quizMode) {
+    if (currentWordIndex >= vocabulary.length) {
+      return (
+        <div>
+          <div>Quiz complete!</div>
+          <div>
+            You scored {score} out of {vocabulary.length}.
+          </div>
+          <button onClick={() => setQuizMode(false)}>Exit quiz</button>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <div>Score: {score}</div>
+        <Flashcard
+          word={shuffledVocabulary[currentWordIndex].word}
+          definition={shuffledVocabulary[currentWordIndex].definition}
+          showDefinition
+        />
+        <ul>
+          {choices.map((item) => (
+            <li key={item.word} onClick={() => handleAnswer(item.word)}>
+              {item.word}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div>
+      <button onClick={handleStartQuiz}>Start quiz</button>
       {shuffledVocabulary.map((item) => (
         <Flashcard
           key={item.word}
